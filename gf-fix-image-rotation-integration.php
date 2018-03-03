4c06360296650d57f7f4053ba22fa88d5d3f4e52
Gagan Deep Singh <me@gagan.pro>
2018-03-04 04:39:56 +0530

    Initial Commit with basic plugin structure

---
+++ gf-fix-image-rotation-integration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: GF Fix Image Rotation Integration
+ * Plugin URI: https://wordpress.org/plugins/fix-image-rotation/
+ * Description: Integrates Fix Image Rotation Plugin with Gravity Forms
+ * Author: Gagan Deep Singh
+ * Version: 1.0.0
+ * Author URI: http://gagan0123.com
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: gf-fix-image-rotation-integration
+ *
+ * @package GF_Fix_Image_Rotation_Integration
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
